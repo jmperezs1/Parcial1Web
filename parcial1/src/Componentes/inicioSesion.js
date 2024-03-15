6dee6b6dbc1d57290b6168754c5faa7233f1a31a
Juan Manuel Perez <jm.perezs1@uniandes.edu.co>
2024-03-15 09:20:09 -0500

    Inicio Sesion

--- parcial1/src/Componentes/inicioSesion.js
+++ parcial1/src/Componentes/inicioSesion.js
@@ -2,9 +2,34 @@ import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import "./incio.css"
 import imagen from "../Imagenes/inicio.png"
+import { useState } from 'react';
 
 function IncioSesion () {
 
+    const [formValues, setFormValues] = useState({email:"", password:""})
+
+    const handleEmailChange = ((e) => {
+        setFormValues({...formValues, email: e.target.value})
+      });
+     
+    const handlePasswordChange = ((e) => {
+        setFormValues({...formValues, password: e.target.value})
+      });
+
+    function click(){
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formValues)
+        };
+
+        fetch('http://localhost:3001/login', requestOptions)
+            .then(response => {
+                console.log(response)
+            });
+
+    }
 
     return (
         <div>
@@ -13,15 +38,15 @@ function IncioSesion () {
         <Form>
                     <Form.Group className="ancho" controlId="formBasicEmail">
                         <Form.Label className="formato">USUARIO</Form.Label>
-                        <Form.Control type="email" placeholder="Ingrese el usuario"/>
+                        <Form.Control type="email" placeholder="Ingrese el usuario"  onChange={handleEmailChange}/>
                     </Form.Group>
                 
                     <Form.Group className="ancho" controlId="formBasicPassword">
                         <Form.Label className="formato">CONTRASEÑA</Form.Label>
-                        <Form.Control type="password" placeholder="Ingrese la clave"/>
+                        <Form.Control type="password" placeholder="Ingrese la clave" onChange={handlePasswordChange}/>
                     </Form.Group>
                     <Form.Group className="text-center margen">
-                        <Button variant="secondary"  className="text-center">Iniciar Sesión</Button>
+                        <Button variant="secondary"  className="text-center" onClick={click}>Iniciar Sesión</Button>
                     </Form.Group>
             
                 </Form>
